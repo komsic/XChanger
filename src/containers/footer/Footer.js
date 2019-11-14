3588087e4fcdfc4d0416e76a72af0397b3910271
komsic <kayodemicheal159@gmail.com>
2019-11-14 21:13:22 +0100

    feature(footer-container): add footer container

---
+++ src/containers/footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="large">
+      Made with
+      {' '}
+      <span>&#10084;</span>
+      {' '}
+      by komsic
+    </p>
+  </footer>
+);
+
+export default Footer;
