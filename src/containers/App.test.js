023dc553b28fd2986c1ed8c8136fea9f5e01fd0f
komsic <kayodemicheal159@gmail.com>
2019-11-16 17:07:25 +0100

    feature(wiring-together): add up the containers to create the app [Finishes #169796766]

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ProviderTest from '../ProviderTest';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+    <ProviderTest>
+      <App />
+    </ProviderTest>), div);
   ReactDOM.unmountComponentAtNode(div);
 });
